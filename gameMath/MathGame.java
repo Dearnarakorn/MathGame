2e7a2ca3f6d9258a912c82ab46887f573cf4586e
Dearnarakorn <madear5659@gmail.com>
2023-02-17 14:06:07 +0700

    can witch page

---
+++ gameMath/MathGame.java
@@ -0,0 +1,15 @@
+package gameMath;
+
+
+public class MathGame {
+
+    public MathGame(){
+        new Homepage();
+        //Mathpage Mathp = new Mathpage();
+
+    }
+
+    public static void main(String[] args) {
+        new MathGame();
+    }
+}
